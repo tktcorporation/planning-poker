702354bc8dfa9a919c10f003a3cd5cba80dd704b
tktcorporation <tktcorporation.go@gmail.com>
2021-11-13 03:07:23 +0900

    :art: fix linting

--- src/pages/poker/_internal/Cards.tsx
+++ src/pages/poker/_internal/Cards.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box, Flex } from "rebass/styled-components";
-import { theme } from "../../../theme";
 import { Card } from "./Card";
 
 export const Cards = (): JSX.Element => {
@@ -8,10 +7,15 @@ export const Cards = (): JSX.Element => {
     <Box>
       <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
         <Box>
-          <Card n={1} borderColor={'primary'} selected={false} marginX={3}></Card>
-          <Card n={1} borderColor={'primary'} selected={false}></Card>
-          <Card n={1} borderColor={'primary'} selected={true}></Card>
-          <Card n={1} borderColor={'primary'} selected={false} ></Card>
+          <Card
+            n={1}
+            borderColor={"primary"}
+            selected={false}
+            marginX={3}
+          ></Card>
+          <Card n={1} borderColor={"primary"} selected={false}></Card>
+          <Card n={1} borderColor={"primary"} selected={true}></Card>
+          <Card n={1} borderColor={"primary"} selected={false}></Card>
         </Box>
       </Flex>
     </Box>
