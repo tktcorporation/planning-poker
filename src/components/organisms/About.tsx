78ef2bd6033bdefca7686cda8de1760f8e2504ce
tktcorporation <tktcorporation.go@gmail.com>
2021-11-14 03:45:49 +0900

    :bug: fix link struct

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Box, Flex, Link as RebassLink } from "rebass/styled-components";
+import { Box, Flex } from "rebass/styled-components";
 import ReactMarkdown from "react-markdown";
 import { Fade } from "react-awesome-reveal";
 import { Section } from "../molecules/Section";
 import { MarkdownRenderer } from "../molecules/MarkdownRenderer";
 import { Triangle } from "../molecules/Triangle";
 
-const vision = `## Simple Planning`;
-
 const purpose = `Plan simply`;
 
 export const About: React.FC = () => {
   return (
     <Section.Container Background={Background}>
-      <Section.Header name={"Planning Poker"} icon="" label="title" href="/poker"/>
       <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
         <Box width={[1, 1, 3 / 5]} px={[2, 3, 4]} mt={2}>
           <Fade direction="down" triggerOnce>
-            <ReactMarkdown source={vision} renderers={MarkdownRenderer} />
+            <Section.Header
+              name={"Planning Poker"}
+              icon=""
+              label="title"
+              href="/poker"
+            />
           </Fade>
           <Fade direction="down" triggerOnce>
             <ReactMarkdown source={purpose} renderers={MarkdownRenderer} />
